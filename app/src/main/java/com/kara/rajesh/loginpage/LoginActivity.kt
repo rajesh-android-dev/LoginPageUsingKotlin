ea522c3c50046ee5e993e09b6b7601764b350561
Rajesh <urajesh.android@gmail.com>
2018-02-09 17:41:13 +0530

    Functionality integrated
    Views declared in java and a simple toast shown for button click

--- app/src/main/java/com/kara/rajesh/loginpage/LoginActivity.kt
+++ app/src/main/java/com/kara/rajesh/loginpage/LoginActivity.kt
@@ -2,11 +2,22 @@ package com.kara.rajesh.loginpage
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        val etUserName = findViewById<EditText>(R.id.etUsername)
+        val etPassword = findViewById<EditText>(R.id.etPassword)
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+        btnLogin.setOnClickListener(View.OnClickListener {
+            Toast.makeText(this,"Logged in",Toast.LENGTH_SHORT).show()
+        })
+
     }
 }
