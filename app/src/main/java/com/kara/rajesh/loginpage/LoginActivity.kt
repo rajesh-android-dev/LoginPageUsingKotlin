136ef6a503d3496cbe90b75600b72c8ea19a7ed8
Rajesh <urajesh.android@gmail.com>
2018-02-12 18:39:22 +0530

    Functionality
    Login functionality compeleted. An alert dialog will be shown after login and null validation also checked.

--- app/src/main/java/com/kara/rajesh/loginpage/LoginActivity.kt
+++ app/src/main/java/com/kara/rajesh/loginpage/LoginActivity.kt
@@ -1,23 +1,40 @@
 package com.kara.rajesh.loginpage
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
+import android.support.v7.app.AppCompatActivity
+import android.text.TextUtils
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-        val etUserName = findViewById<EditText>(R.id.etUsername)
-        val etPassword = findViewById<EditText>(R.id.etPassword)
-        val btnLogin = findViewById<Button>(R.id.btnLogin)
         btnLogin.setOnClickListener(View.OnClickListener {
-            Toast.makeText(this,"Logged in",Toast.LENGTH_SHORT).show()
+            performLogin()
         })
 
+    }
+
+
+       private fun performLogin() {
+        if (!TextUtils.isEmpty(etUsername.text.toString()) && !TextUtils.isEmpty(etPassword.text.toString())) {
+            val successAlert = AlertDialog.Builder(this).create()
+            successAlert.setTitle("Success")
+            successAlert.setMessage("Logged in Successfully")
+            successAlert.setButton(AlertDialog.BUTTON_POSITIVE,"OK",{
+                dialogInterface, i ->
+                Toast.makeText(this,"Success",Toast.LENGTH_SHORT).show()
+            })
+            successAlert.show()
+        } else {
+            Toast.makeText(this, "Please eneter all the values", Toast.LENGTH_SHORT).show()
+        }
     }
 }
---
+++ app/src/main/java/com/kara/rajesh/loginpage/PatternUtil.kt
@@ -0,0 +1,11 @@
+package com.kara.rajesh.loginpage
+
+import java.util.regex.Pattern
+
+/**
+ * Created by Apple on 12/02/18.
+ */
+class PatternUtil constructor()
+{
+
+}
